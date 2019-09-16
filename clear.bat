337e4d81a03a83abff43777a3a3239522c70fda3
lxz1104 <1366499939@qq.com>
2019-09-16 15:13:48 +0800

    清除无用文件,添加项目无用文件清理脚本

---
+++ clear.bat
@@ -0,0 +1,52 @@
+del *.hdb /s
+del *.cdb /s
+del *.tdf /s
+del *.ddb/s
+del *.qmsg /s
+del *.hsd /s
+del *.rdb /s
+del *.ammdb /s
+::del *.opt /s  ::不允许删除JLINK的设置
+del *.bpm/s
+del *.tdb /s
+del *.hier_info /s
+del *.db_info/s
+del *.hif/s
+del *.idb /s
+del *.ipinfo /s
+del *.kpt/s
+del *.logdb /s
+del *.sci/s
+del *.syn_hier_info /s
+del *.xml /s
+del *.rpt /s
+del *.wlf /s
+del *.sdo /s
+del *.vo /s
+del *.xrf /s       
+del *.dbs /s
+del *.psm /s
+del *.qws /s
+del *.sft /s       
+
+
+
+
+
+del *.qws /s
+del *.sft /s       
+del *.qws /s
+del *.sft /s       
+del *.qws /s
+del *.sft /s       
+
+
+
+
+del JLinkLog.txt /s
+
+del *.iex /s
+del *.htm /s
+del *.sct /s
+del *.map /s
+exit
